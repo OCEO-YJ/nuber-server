998e288fbc9c7bb63b58f4931c920e425cc35c84
OCEO <oceo.yj@gmail.com>
2018-10-14 13:13:21 -0700

    Try Commit

---
+++ src/api/Hello/sayBye/sayBye.resolvers.ts
@@ -0,0 +1,7 @@
+const resolvers = {
+    Query: {
+        sayBye: ()=> "OCEO"
+    }
+}
+
+export default resolvers;
---
+++ src/api/Hello/sayHello/sayHello.resolvers.ts
@@ -0,0 +1,7 @@
+const resolvers = {
+    Query: {
+        sayHello: ()=> "OCEO"
+    }
+}
+
+export default resolvers;
